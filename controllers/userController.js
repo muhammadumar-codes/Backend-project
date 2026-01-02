6f62dc1a2ca1b706ea455130df440b6f4d07918a
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-01-02 18:55:28 +0500

    add the feature to add new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,8 +19,42 @@ const userById = (req, res) => {
   res.json({ isSuccess: true, user })
 }
 
+// creating Users
+
+const createUser = (req, res) => {
+  const { name, brand } = req.body
+
+  // checking users name and brand
+
+  if (!name || !brand) {
+    return res.status(404).json({
+      isSuccess: false,
+      message: 'username and Email is Required ',
+    })
+  }
+
+  // now creating new users
+  const newUser = {
+    id: users.length + 1,
+    name,
+    brand,
+  }
+
+  // adding user to the users arrays
+  users.push(newUser)
+
+  // message after adding the usersto the data
+
+  res.status(200).json({
+    isSuccess: true,
+    message: 'user Successfully Created',
+    data: newUser,
+  })
+}
+
 // Exporting
 module.exports = {
   allUsers,
   userById,
+  createUser,
 }
--- routes/user.Routes.js
+++ routes/user.Routes.js
@@ -3,11 +3,16 @@ const router = express.Router()
 
 // imported all exported code from the usercontrollers
 
-const { allUsers, userById } = require('../controllers/userController')
+const {
+  allUsers,
+  userById,
+  createUser,
+} = require('../controllers/userController')
 
 // users Router
 
 router.get('/', allUsers)
 router.get('/:id', userById)
+router.post('/', createUser)
 
 module.exports = router
